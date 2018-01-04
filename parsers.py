62b1edc801ddd6377c0481dafd3e0bf26ba5ba5d
Tomas Kratochvil <tomas.kratochvil@eledus.cz>
2018-01-04 23:40:42 +0100

    KansasJidelna regex parser is half working

--- parsers.py
+++ parsers.py
@@ -17,33 +17,21 @@ def GetMenuKanasJidelna(day):
         r.encoding = 'utf-8'
 
         soup = BeautifulSoup(r.text, "html5lib")  # Gets html code
-        menu = soup.findAll("div", {"class", "menu-list menu-list__dotted"})
+        menu = soup.findAll("div", {"class", "nabidka-in tab_container"})
         menu_extracted = sub(r'[\t\n\r]', '', str(menu))
+        print(menu_extracted)
+        menu_extracted = findall(r"id=\"tab2(.*?)class=\"cle", menu_extracted, DOTALL)
+        print(menu_extracted[0])
 
-        menu_courses_czech = findall(r"content\">(.*?)</", menu_extracted_day[0], DOTALL)
-        menu_courses_orig = findall(r"_title\">(.*?)</", menu_extracted_day[0], DOTALL)
-        menu_courses = menu_courses_orig
-        for i in range(len(menu_courses_orig)):
-            menu_courses[i] = menu_courses_orig[i] + " - " + menu_courses_czech[i]
+        menu_courses = findall(r"content\">(.*?)</", menu_extracted_day[0], DOTALL)
         menu_prices = findall(r"price\">(.*?)</", menu_extracted_day[0], DOTALL)
 
-        kanasJidelnaTemp["Polévka"] = menu_courses[0] + " " + menu_prices[0]
+        kanasJidelnaTemp["Polévka 1"] = menu_courses[0] + " " + menu_prices[0]
+        kanasJidelnaTemp["Polévka 2"] = menu_courses[0] + " " + menu_prices[0]
 
         kanasJidelnaTemp["Menu 1"] = dict()
         kanasJidelnaTemp["Menu 1"]["menu"] = menu_courses[1]
         kanasJidelnaTemp["Menu 1"]["cena"] = menu_prices[1]
-
-        kanasJidelnaTemp["Menu 2"] = dict()
-        kanasJidelnaTemp["Menu 2"]["menu"] = menu_courses[2]
-        kanasJidelnaTemp["Menu 2"]["cena"] = menu_prices[2]
-
-        kanasJidelnaTemp["Menu 3"] = dict()
-        kanasJidelnaTemp["Menu 3"]["menu"] = menu_courses[3]
-        kanasJidelnaTemp["Menu 3"]["cena"] = menu_prices[3]
-
-        kanasJidelnaTemp["Menu 4"] = dict()
-        kanasJidelnaTemp["Menu 4"]["menu"] = menu_courses[4]
-        kanasJidelnaTemp["Menu 4"]["cena"] = menu_prices[4]
 
         kanasJidelna = kanasJidelnaTemp
     except:
