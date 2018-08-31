0a7999368972f73629e6c11fb7f3f8336b4ba057
ToKra <kratas.tom@gmail.com>
2018-08-31 14:52:39 +0200

    add import to time checker file

--- correct_time_checker.py
+++ correct_time_checker.py
@@ -1,6 +1,10 @@
-def check_if_menu_can_be_posted():    
-    today_is_holiday = datetime.date(datetime.now()) in holidays:
-    today_is_working_day = datetime.today().weekday() == 5 or datetime.today().weekday() == 6:
+from czech_holidays import holidays
+from datetime import datetime
+
+
+def check_if_menu_can_be_posted():
+    today_is_holiday = datetime.date(datetime.now()) in holidays
+    today_is_working_day = datetime.today().weekday() == 5 or datetime.today().weekday() == 6
     menu_can_be_posted = today_is_holiday and today_is_working_day
     
     return menu_can_be_posted
