c549f083b4a7ae925858274b682f159ea60c7d19
ToKra <kratas.tom@gmail.com>
2018-08-31 14:12:49 +0200

    clean main.py

--- main.py
+++ main.py
@@ -3,60 +3,30 @@
 
 from re import search, findall, DOTALL, sub
 from requests import get, post
-from json import dumps
-from datetime import datetime
 from bs4 import BeautifulSoup
 from sys import exit
-from czech_holidays import holidays
 from parsers import GetMenuKanas, GetMenuPurkynka
 import sys
-import argparse
 import random
+
+import json
+import argparse
 
 
 if __name__ == "__main__":
-    day = datetime.today().weekday()
-    time = datetime.now().time()
-
-
-
-
-    with open('gliplinks.txt') as f:
-        url_list = f.readlines()
-
-    with open('local_fortunecookie.txt') as ff:
-        local_fortunecookie = ff.readlines()
-
-    url_test = url_list[2][:-1]
-    url_conv = url_list[4][:-1]
-    url = ''
-
     ap = argparse.ArgumentParser()
-    ap.add_argument("--type", required = True,
-	help = "Type of posting conversation t/o")
-    ap.add_argument("--postfunc", required = True,
-	help = "Name of posting function.")
+    ap.add_argument("--type", required=True, help="Type of posting conversation.")
     args = vars(ap.parse_args())
 
-    if args["type"] == 't':
-        url = url_test
-    elif args["type"] == 'o':
-        url = url_conv
+    with open('glip_links.json') as file:
+        glip_links = json.load(file)
 
-    if args["postfunc"] in (str(x.__name__) for x in get_menu_func_list):
-        try:
-            if args["postfunc"] == 'PostFortuneCookie':
-                PostFortuneCookie(url)
-            else:
-                PostMenu(getattr(sys.modules[__name__], args["postfunc"])(day), url)
-        except:
-            print("Posting of {} failed!".format(args["postfunc"]))
-    else:
-        for func in get_menu_func_list:
-            try:
-                if str(func.__name__) == 'PostFortuneCookie':
-                    PostFortuneCookie(url)
-                else:
-                    PostMenu(func(day), url)
-            except:
-                print("{} posting failed.".format(func.__name__) )
+    with open('local_fortunecookies.json') as file:
+        local_fortunecookies = json.load(file)
+
+    with open('restaurant_links.json') as file:
+        restaurant_links = json.load(file)
+
+    post_url = glip_links[args["type"]]
+
+
