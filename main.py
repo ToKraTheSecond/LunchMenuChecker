b0b400f563cb8d00f7081697b8c4c2ce3c7fec2c
ToKra <kratas.tom@gmail.com>
2018-09-21 18:43:48 +0200

    check correct posting day in main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 ﻿import argparse
 from types import SimpleNamespace
+from sys import exit
 
 from posters import post_fortune_cookie, post_menu
 from parsers import get_menu_nepal
 from json_files_handlers import load_data_from_json_files
+from correct_time_checker import check_if_menu_can_be_posted
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("--type", required=True, help="Type of posting conversation.")
     args = vars(ap.parse_args())
+
+    if not check_if_menu_can_be_posted():
+        exit('Not posting on non working day')
 
     data_from_json_files = load_data_from_json_files()
     data_from_json_files = SimpleNamespace(**data_from_json_files)
