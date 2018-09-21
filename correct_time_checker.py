b199ee4d93bd7b7cd7487b5f2963e79502f7194c
ToKra <kratas.tom@gmail.com>
2018-09-21 18:38:22 +0200

    correct time checker bug

--- correct_time_checker.py
+++ correct_time_checker.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 
 def check_if_menu_can_be_posted():
-    today_is_holiday = datetime.date(datetime.now()) in holidays
-    today_is_working_day = datetime.today().weekday() == 5 or datetime.today().weekday() == 6
-    menu_can_be_posted = today_is_holiday and today_is_working_day
+    today_is_not_holiday = datetime.date(datetime.now()) not in holidays
+    today_is_working_day = datetime.today().weekday() not in [5, 6]
+    menu_can_be_posted = today_is_not_holiday and today_is_working_day
     
     return menu_can_be_posted
